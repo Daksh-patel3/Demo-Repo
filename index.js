4eebf0542d10c6c008405def2fbf7c26de5c912b
Daksh Patel <daksh6928@gmail.com>
2024-07-16 18:14:54 +0530

    Adding Project Files

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
-require("dotenv").config()
+const express=require("express");
 
-const express = require("express");
-const app = express();
-const port = 4000;
+const app=express()
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
+app.listen(3000);
 
-app.get("/twitter",(req,res)=>{
-    res.send("dakshdotcom")
+app.use(express.json())
+
+const users=[{
+    name:"john",
+    kidneys:[{
+        healthy:false
+    }]
+}]
+
+app.get("/",(req,res)=>{
+ let johnkedneys=users[0].kidneys;
+ let TotalJohnKidneys=johnkedneys.length
+
+ let HealthyJohnKidneys=0;
+ for(i=0;i<johnkedneys.length;i++){
+    if(johnkedneys[i].healthy){
+        HealthyJohnKidneys=HealthyJohnKidneys+1;
+    }
+ }
+
+ let UnhealthyJohnKidneys=TotalJohnKidneys-HealthyJohnKidneys;
+
+ res.json({TotalJohnKidneys,HealthyJohnKidneys,UnhealthyJohnKidneys})
 })
 
-app.get("/login",(req,res)=>{
-    res.send("please do login...")
+app.post("/",(req,res)=>{
+    let isHealthy=req.body.isHealthy
+    users[0].kidneys.push({healthy:isHealthy})
+
+    res.json({
+        "msg":"Done!"
+    })
 })
 
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}!`);
-});
+app.put("/",(req,res)=>{
+    for(let i=0;i<users[0].kidneys.length;i++){
+        users[0].kidneys[i].healthy=true
+    }
+
+    res.json({})
+})
+
+app.delete("/",(req,res)=>{
+    let newArr=[];
+    for(let i=0;i<users[0].kidneys.length;i++){
+        if(users[0].kidneys[i].healthy){
+            newArr.push(users[0].kidneys[i])
+        }
+    }
+    users[0].kidneys=newArr;
+    res.json({})
+})
